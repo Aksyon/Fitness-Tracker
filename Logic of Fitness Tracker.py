d62b02531a0a8fb50ff8e286ad2244548f03e8d3
Aksyon <aksenovaleksander94@gmail.com>
2022-08-28 17:24:09 +0500

    Created class InfoMessage, read_package and main methods

--- Logic of Fitness Tracker.py
+++ Logic of Fitness Tracker.py
@@ -1,11 +1,11 @@
 
 
+from turtle import distance, speed
+
+
 class Training():
     """Basic class for different sport activities."""
-    LEN_STEP = {'SWM': 1.38,
-                'RUN': 0.65,
-                'WLK': 0.65
-    }
+    LEN_STEP = 0.65
     M_IN_KM = 1000
     H_IN_MIN = 60 
 
@@ -17,14 +17,14 @@ class Training():
     def get_distance(self) -> float:
         return self.action * self.LEN_STEP / self.M_IN_KM
 
-    def get_mean_speed(self):
+    def get_mean_speed(self) -> float:
         return self.get_distance() / self.duration
     
     def get_spent_calories(self):
         pass
-    
-    def show_training_info():
-        pass
+    @staticmethod
+    def show_training_info(self):
+        return InfoMessage(self)
 
 
 class Running(Training):
@@ -32,7 +32,7 @@ class Running(Training):
     COEF_CALORIE_1 = 18
     COEF_CALORIE_2 = 20
     
-    def get_spent_calories(self):
+    def get_spent_calories(self) -> float:
         return ((self.COEF_CALORIE_1 * self.get_mean_speed() -
                 self.COEF_CALORIE_2) * self.weight / self.M_IN_KM *
                 self.duration * self.H_IN_MIN)
@@ -44,11 +44,11 @@ class SportsWalking(Training):
     COEF_CALORIES_2 = 0.029
     
 
-    def __init__(self, height):
+    def __init__(self, height: float):
         super().__init__(self)
         self.height = height
     
-    def get_spent_calories(self):
+    def get_spent_calories(self) -> float:
         return ((self.COEF_CALORIES_1 * self.weight + (self.get_mean_speed()**2
                // self.height) * self.COEF_CALORIES_2 * self.weight) *
                self.duration * self.H_IN_MIN)
@@ -58,16 +58,49 @@ class Swimming(Training):
     """Class describe methods and parameters for swimming activity."""
     COEF_CALORIES_1 = 1.1
     COEF_CALORIES_2 = 2
+    LEN_STEP = 1.38
 
-    def __init__(self, length_pool, count_pool):
+    def __init__(self, length_pool: int, count_pool: int):
         super.__init__(self)
         self.length_pool = length_pool
         self.count_pool = count_pool
     
-    def get_mean_speed(self):
+    def get_mean_speed(self) -> float:
         return ((self.length_pool * self.count_pool) /
                self.M_IN_KM / self.duration)
     
-    def get_spent_calories(self):
+    def get_spent_calories(self) -> float:
         return ((self.get_mean_speed() + self.COEF_CALORIES_1) *
                self.COEF_CALORIES_2 * self.weight)
+
+
+class InfoMessage():
+    """Class describe info object."""
+    def __innit__(self):
+        training_type = self.class_name
+        duration = self.duration
+        distance = self.get_distance()
+        speed = self.get_mean_speed()
+        calories = self.get_spent_calories
+    
+    @staticmethod
+    def printing(self):
+        return (f'Тип тренировки: {self.training_type};'
+                f'Длительность: {self.duration} ч.;'
+                f'Дистанция: {self.distance} км;'
+                f'Ср. скорость: {self.speed} км/ч;'
+                f'Потрачено ккал: {self.calories}.')
+
+
+def read_package(workout_type, data):
+    if workout_type == 'SWM':
+        activity = Swimming(data[0], data[1], data[2], data[3], data[4])
+    if workout_type == 'RUN':
+        activity = Running(data[0], data[1], data[2])
+    if workout_type == 'WLK':
+        activity = SportsWalking(data[0], data[1], data[2], data[3])
+    return activity
+
+def main(training):
+    info = Training.show_training_info(training)
+    print(InfoMessage.printing(info))
